dff22f673d491e5d9c2d7c924b67c4dfbd847f3f
AlexisWilke <alexis@m2osw.com>
2022-05-17 18:08:10 -0700

    Cleaned up the cmake file.

--- cmake/FtMeshConfig.cmake
+++ cmake/FtMeshConfig.cmake
@@ -1,37 +1,36 @@
-# The following variables are defined with the FtMesh library parameters:
+# - Find FtMesh
 #
 # FTMESH_FOUND        - System has FtMesh
 # FTMESH_INCLUDE_DIRS - The FtMesh include directories
-# FTMESH_LIBRARIES    - The libraries needed to use FtMesh (none)
-# FTMESH_DEFINITIONS  - Compiler switches required for using FtMesh (none)
+# FTMESH_LIBRARIES    - The libraries needed to use FtMesh
+# FTMESH_DEFINITIONS  - Compiler switches required for using FtMesh
 #
 # License:
-#   Copyright (c) 2021-2022  Made to Order Software Corp.  All Rights Reserved
 #
-#   https://snapwebsites.org/
-#   contact@m2osw.com
-# 
-#   This program is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
+# Copyright (c) 2011-2022  Made to Order Software Corp.  All Rights Reserved
 #
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
+# https://snapwebsites.org/project/ftmesh
+# contact@m2osw.com
 #
-#   You should have received a copy of the GNU General Public License along
-#   with this program; if not, write to the Free Software Foundation, Inc.,
-#   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 find_path(
     FTMESH_INCLUDE_DIR
         ftmesh/mesh.h
 
     PATHS
-        $ENV{FTMESH_INCLUDE_DIR}
+        ENV FTMESH_INCLUDE_DIR
 )
 
 find_library(
@@ -39,7 +38,8 @@ find_library(
         ftmesh
 
     PATHS
-        $ENV{FTMESH_LIBRARY}
+        ${FTMESH_LIBRARY_DIR}
+        ENV FTMESH_LIBRARY
 )
 
 mark_as_advanced(
@@ -48,18 +48,15 @@ mark_as_advanced(
 )
 
 set(FTMESH_INCLUDE_DIRS ${FTMESH_INCLUDE_DIR})
-set(FTMESH_LIBRARIES    ${FTMESH_LIBRARY}    )
+set(FTMESH_LIBRARIES    ${FTMESH_LIBRARY})
 
 
-# handle the QUIETLY and REQUIRED arguments and set FTMESH_FOUND to TRUE
-# if all listed variables are TRUE
-#
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
     FtMesh
-    DEFAULT_MSG
-    FTMESH_INCLUDE_DIR
-    FTMESH_LIBRARY
+    REQUIRED_VARS
+        FTMESH_INCLUDE_DIR
+        FTMESH_LIBRARY
 )
 
 # vim: ts=4 sw=4 et
